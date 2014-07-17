49b8949c2ec3f65054ddd8937d8fc8aa8f9da807
Jonathan Kempf <kempfjj@gmail.com>
2014-07-16 22:39:38 -0400

    Protecting name spaces my modularizing the JS

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,8 @@
-function MyController($scope) {
+var myApp = angular.module('myApp', []);
+myApp.controller('MyController', function MyController($scope) {
     $scope.author = {
         'name': 'Jonathan Kempf',
         'title': 'UI/UX Developer/Designer',
         'company': 'vzw.com'
     }
-}
+})
