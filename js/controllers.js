6da1fc59036a127fba46bfa8d6188720438ab5ef
Jonathan Kempf <kempfjj@gmail.com>
2014-07-20 23:45:48 -0400

    Working on some SCSS/CSS, commiting so I will have a revert before I try this out.

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,53 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('MyController', function MyController($scope) {
-    $scope.author = {
-        'name': 'Jonathan Kempf',
-        'title': 'UI/UX Developer/Designer',
-        'company': 'vzw.com'
-    }
+    $scope.developers = [
+		{ "firstName": "Jon",
+		  "lastName": "Kempf",
+		  "email": "kempfjj@gmail.com",
+		  "work": "Verizon Wireless",
+		  "age": "30" 
+		},
+		{ "firstName": "Mark",
+		  "lastName": "Siemba",
+		  "email": "marks@gmail.com",
+		  "work": "Seevast",
+		  "age": "44" 
+		},
+		{ "firstName": "Laura",
+		  "lastName": "Diemer",
+		  "email": "attempestcyclone@gmail.com",
+		  "work": "Cellino and Barnes",
+		  "age": "30" 
+		},
+		{ "firstName": "Jon",
+		  "lastName": "Kempf",
+		  "email": "kempfjj@gmail.com",
+		  "work": "Verizon Wireless",
+		  "age": "30" 
+		},
+		{ "firstName": "Jon",
+		  "lastName": "Kempf",
+		  "email": "kempfjj@gmail.com",
+		  "work": "Verizon Wireless",
+		  "age": "30" 
+		},
+		{ "firstName": "Jon",
+		  "lastName": "Kempf",
+		  "email": "kempfjj@gmail.com",
+		  "work": "Verizon Wireless",
+		  "age": "30" 
+		},
+		{ "firstName": "Jon",
+		  "lastName": "Kempf",
+		  "email": "kempfjj@gmail.com",
+		  "work": "Verizon Wireless",
+		  "age": "30" 
+		},
+		{ "firstName": "Jon",
+		  "lastName": "Kempf",
+		  "email": "kempfjj@gmail.com",
+		  "work": "Verizon Wireless",
+		  "age": "30" 
+		}
+	]
 })
