e72b833fd08961c043eb0d14969453a533a2b25a
Jonathan Kempf <kempfjj@gmail.com>
2014-07-22 16:46:57 -0400

    Removed compass-normalize gem, as it was causing fatal path error on compass compile

--- config.rb
+++ config.rb
@@ -1,5 +1,9 @@
 # Require any additional compass plugins here.
 
+require "compass"
+gem "modular-scale"
+require "modular-scale"
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
