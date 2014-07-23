9f8021c55d038beacf2ff3630389fe417d291f50
Jonathan Kempf <kempfjj@gmail.com>
2014-07-23 00:08:57 -0400

    Modifying controller to use HTTP service. Keep in mind the code changes to pass the scope and http vars into the array to protect from minification

--- js/controllers.js
+++ js/controllers.js
@@ -1,53 +1,6 @@
 var myApp = angular.module('myApp', []);
-myApp.controller('MyController', function MyController($scope) {
-    $scope.developers = [
-		{ "firstName": "Jon",
-		  "lastName": "Kempf",
-		  "email": "kempfjj@gmail.com",
-		  "work": "Verizon Wireless",
-		  "age": "30" 
-		},
-		{ "firstName": "Mark",
-		  "lastName": "Siemba",
-		  "email": "marks@gmail.com",
-		  "work": "Seevast",
-		  "age": "44" 
-		},
-		{ "firstName": "Laura",
-		  "lastName": "Diemer",
-		  "email": "attempestcyclone@gmail.com",
-		  "work": "Cellino and Barnes",
-		  "age": "30" 
-		},
-		{ "firstName": "Jon",
-		  "lastName": "Kempf",
-		  "email": "kempfjj@gmail.com",
-		  "work": "Verizon Wireless",
-		  "age": "30" 
-		},
-		{ "firstName": "Jon",
-		  "lastName": "Kempf",
-		  "email": "kempfjj@gmail.com",
-		  "work": "Verizon Wireless",
-		  "age": "30" 
-		},
-		{ "firstName": "Jon",
-		  "lastName": "Kempf",
-		  "email": "kempfjj@gmail.com",
-		  "work": "Verizon Wireless",
-		  "age": "30" 
-		},
-		{ "firstName": "Jon",
-		  "lastName": "Kempf",
-		  "email": "kempfjj@gmail.com",
-		  "work": "Verizon Wireless",
-		  "age": "30" 
-		},
-		{ "firstName": "Jon",
-		  "lastName": "Kempf",
-		  "email": "kempfjj@gmail.com",
-		  "work": "Verizon Wireless",
-		  "age": "30" 
-		}
-	]
-})
+myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
+	$http.get('js/dummyJson.json').success(function(data) {
+		$scope.developers = data;
+	});
+}]);
