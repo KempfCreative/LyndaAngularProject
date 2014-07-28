985dd14b9476d3c5a5cab837e72f69207648d4c6
Jonathan Kempf <kempfjj@gmail.com>
2014-07-27 23:04:51 -0400

    Binding to input fields through filters

--- js/controllers.js
+++ js/controllers.js
@@ -2,5 +2,6 @@ var myApp = angular.module('myApp', []);
 myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
 	$http.get('js/dummyJson.json').success(function(data) {
 		$scope.developers = data;
+		$scope.developerOrder = "firstName";
 	});
 }]);
