217f661d48c9f743bb47cb142b558c7d44f66ba7
Jonathan Kempf <kempfjj@gmail.com>
2014-07-30 23:27:17 -0400

    Setting up deep linking by separating out controller, adding ng-view

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,5 @@
-var myApp = angular.module('myApp', []);
-myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
+var developerControllers = angular.module('developerControllers', []);
+developerControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
 	$http.get('js/dummyJson.json').success(function(data) {
 		$scope.developers = data;
 		$scope.developerOrder = "firstName";
